93e663efa8cb3063f94671eeb485a32d1e73584c
EC2 Default User <ec2-user@ip-172-31-27-124.us-east-2.compute.internal>
2018-12-12 08:26:35 +0000

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,75 +3,66 @@ myApp.controller("chatController", ["$scope", "$firebaseArray",
  function($scope, $firebaseArray) {
    var ref = firebase.database().ref().child("messages");
    $scope.chats = $firebaseArray(ref);
+   $scope.curUser = null;
    
-    $scope.curUser = null;
- 
+
+
    $scope.register = function(user) {
      console.log("script.js register");
      var badreq = false;
-     angular.element(document.querySelector("#second")).html("");
+     angular.element(document.querySelector("#err")).html("");
+     angular.element(document.querySelector("#third")).html("");
      
      /*check all fields filled out*/     
-     if(user == undefined || user.name == undefined || user.username == undefined || user.password == undefined || user.email == undefined || user == undefined || user.name == "" || user.username == "" || user.password == "" || user.email == ""){
-       angular.element(document.querySelector("#second")).html("<p>please enter all 4 fields</p>");
+     if(user == undefined || user.name == undefined || user.username == undefined || user.password == undefined || user.email == undefined 
+     || user.name == "" || user.username == "" || user.password == "" || user.email == ""){
+       angular.element(document.querySelector("#err")).html("<p>please enter all 4 fields</p>");
        badreq = true;
      }
      
+      
+      
+      var message = "<p>successfully registered!</p>";
       /*check user doesn't already exist*/
-     ref.on("value", function(snapshot) {
+      ref.on("value", function(snapshot) {
       snapshot.forEach(function (child){
+
         if(child.val().username == user.username){
           badreq = true;
-          angular.element(document.querySelector("#second")).html("<p>username already in system. Pick new username or try logging in</p>");
+          message = "<p>username already in system. Pick new username or try logging in</p>";
         }
         else if(child.val().email == user.email){
           badreq = true;
-          angular.element(document.querySelector("#second")).html("<p>email already in system. Pick new email or try logging in</p>");
+          message = "<p>email already in system. Pick new email or try logging in</p>";
         }
       })
      }, function (errorObject) {
-     angular.element(document.querySelector("#second")).html("<p>internal error :-( pls try again</p>");
+     message = "<p>internal error :-( pls try again</p>";
      badreq = true;
-     console.log("The read failed: " + errorObject.code);
      });
    
-   
+     angular.element(document.querySelector("#err")).html(message);
+     
      if(!badreq){
-       var newmessage = {name:user.name, username:user.username, password:user.password, email:user.email}; //   var newmessage = {from:user.name || "anonymous",body:user.chat};
+       var newmessage = {name:user.name, username:user.username, password:user.password, email:user.email};
        $scope.chats.$add(newmessage);
-       user.name = "";
-       user.username = "";
-       user.password = "";
-       user.email = "";
+       $scope.curUser = user;
      }
-   }
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   $scope.go = function(user) {
-     console.log("go");  
-     angular.element(document.querySelector("#first")).html("<p>sup</p>");
-
+     user.name = "";
+     user.username = "";
+     user.password = "";
+     user.email = "";
    }
    
    
    $scope.login = function(user) {
      console.log("script.js login");
      var badreq = false;
-     angular.element(document.querySelector("#second")).html("");
+     angular.element(document.querySelector("#err")).html("");
      
      /*check data was inputted*/
      if(user == undefined || user.username == "" || user.password == ""){
-       angular.element(document.querySelector("#second")).html("<p>please enter username and password</p>");
+       angular.element(document.querySelector("#err")).html("<p>please enter username and password</p>");
        badreq = true; 
      }
      var exists = false;
@@ -82,29 +73,71 @@ myApp.controller("chatController", ["$scope", "$firebaseArray",
         }
        })
      }, function (errorObject) { 
-     console.log("The read failed: " + errorObject.code);
-       angular.element(document.querySelector("#second")).html("<p>internal error. Please try again</p>");
+       angular.element(document.querySelector("#err")).html("<p>internal error. Please try again</p>");
        badreq = true;
      });
  
      if(!exists)
-       angular.element(document.querySelector("#second")).html("<p>Incorrect username or password</p>");
-     else
-        $scope.go(user);
+       angular.element(document.querySelector("#err")).html("<p>Incorrect username or password</p>");
+     else{
+         $scope.curUser = user;
+         return true;
+     }
+     return false;
    }//end login
  
+    $scope.loginView = function(user) {
+        console.log("loginView");  
+        if(!$scope.login(user))
+            return;
+        angular.element(document.querySelector("#err")).html("");
+        angular.element(document.querySelector("#third")).html("");
 
-    $scope.setUpLogReg = function(){
-        var lr = " <div ng-controller=\"chatController\"> <div class=\"row\"> <div class=\"column\" id=\"col1\">"
-      + "<form novalidate class=\"simple-form\"> Username: <input type=\"text\" ng-model=\"user.username\" /><br />"
-      + "Password: <input type=\"password\" ng-model=\"user.password\" /><br /> <input type=\"submit\" ng-click=\"login(user)\" value=\"Login\" />"
-      + "</form> </div> <div class=\"column\" id=\"col2\"> <form novalidate class=\"simple-form\">"
-      + "Name: <input type=\"text\" ng-model=\"user.name\" /><br /> Username: <input type=\"text\" ng-model=\"user.username\" /><br />"
-      + "Email: <input type=\"text\" ng-model=\"user.email\" /><br /> Password: <input type=\"password\" ng-model=\"user.password\" /><br />"
-      + "<input type=\"submit\" ng-click=\"register(user)\" value=\"Register\" /></form></div></div>";
-      angular.element(document.querySelector("#first")).html(lr);
+        
+            
+        var everything = "<h1>" + $scope.curUser.username + "'s posts</h1>"
+        angular.element(document.querySelector("#err")).html(everything);
+
+        
+        
+        everything = "<ul>";
+        var exists = false;
+        ref.on("value", function(snapshot) {
+            snapshot.forEach(function (child){
+                if(child.val().username == $scope.curUser.username && child.val().date != null){
+                    everything+= "<li>" + child.val().date + ": " + child.val().entry;
+                }
+            })
+        }, function (errorObject) { 
+        angular.element(document.querySelector("#err")).html("<p>internal error. Please try again</p>");
+     });
+        
+     angular.element(document.querySelector("#third")).html(everything+"</ul>");
+
+        
+        
+    }
+    
+    $scope.loginPost = function(user) {
+        angular.element(document.querySelector("#err")).html("");
+        angular.element(document.querySelector("#third")).html("");
+        if(!$scope.login(user))
+            return;
+        if(user == undefined || user.date == undefined || user.entry == undefined || user.date == "" || user.entry == ""){
+            angular.element(document.querySelector("#err")).html("<h2>Post was not added. Please fill out ALL fields.</h2>");
+            return;
+        }
+        
+        var newmessage = {username:user.username, date:user.date, entry:user.entry}; 
+        $scope.chats.$add(newmessage);
+        angular.element(document.querySelector("#err")).html("<h2>Post added. Login & View to see your posts!</h2>");
     }
 
+ 
+ 
+ 
+
+  
 
  }
 
